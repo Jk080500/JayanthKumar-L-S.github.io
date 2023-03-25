6be92e0cf53ab8e3f9266fca1f846ad27dca4faf
Jk080500 <92149113+Jk080500@users.noreply.github.com>
2023-03-25 14:50:44 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -3,14 +3,15 @@
 
 $email= $_POST['email'];
 $message= $_POST['message'];
-$to = "jayanthkumar0805@mail.com";
+$to = "jayanthkumar0805@gmail.com";
 $subject = "Mail From website";
 $Email = " . $email . "\r\n Message =" . $message;
 $headers = "From: noreply@jkdev.com" . "\r\n" .
 "CC: somebodyelse@example.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
+    header("Location:Thankyou.html");
 }
 //redirect
-header("Location:Thankyou.html");
+//
 ?>
